ac3c41a4f03eec881f86918a1fcbe1ab9e9ccc58
Ramasubramaniam <iiscrams@gmail.com>
2024-11-12 15:41:07 +0530

    Update co_raw_data_process.py

--- co_raw_data_process.py
+++ co_raw_data_process.py
@@ -254,11 +254,12 @@ if uploaded_file is not None:
                             threshold=threshold, 
                             method=method.lower()
                         )
-                        st.write("Attainment Summary:")
+                       
                         
                         # Display the summary DataFrame
                         st.write("### Summary of Course Outcomes")
-                        st.write("### Current Attainment Levels: 1 if Attainment score Less than 60% Students attain the outcome, 2 if Attainment score if between 60% and 80% students attain outcome, 3 if more than 80% achieve the outcome.")
+                        st.write(f"### Attainment Levels: 1 if Attainment score Less than {attain_level_2_min}% Students attain the outcome, 2 if Attainment score if between {attain_level_2_min}% and {attain_level_3_min}% students attain outcome, 
+                                3 if more than {attain_level_3_min}% achieve the outcome.")
                         
                         st.dataframe(summary_df)
 
